2ba98b44ceee7f4f0a6257a4e9dacc1072dc0880
Oyelowo <oyelowooyedayo@gmail.com>
2018-10-12 10:43:19 +0300

    reducer set up

---
+++ src/store/reducers/places.js
@@ -0,0 +1,56 @@
+import {
+  ADD_PLACE,
+  DELETE_PLACE,
+  SELECT_PLACE,
+  DESELECT_PLACE
+} from "../actions/actionTypes";
+
+const initialState = {
+  places: [],
+  selectedPlace: null
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_PLACE:
+      return {
+        ...state,
+        places: state.places.concat({
+          key: Math.random().toString(),
+          placeName: action.placeName,
+          placeImage: {
+            uri:
+              "http://www.helsinki.fi/kirjasto/files/2814/0005/0986/kumpula.jpg"
+          }
+        })
+      };
+
+    case DELETE_PLACE:
+      return {
+        ...state,
+        places: state.places.filter(place => {
+          return place.key !== state.selectedPlace.key;
+        }),
+        selectedPlace: null
+      };
+
+    case SELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: state.places.find(place => {
+          return place.key === action.key;
+        })
+      };
+
+    case DESELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: null
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
