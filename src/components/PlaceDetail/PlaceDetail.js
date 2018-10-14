3f7baa296d76f86448b880711c359a4fb2bebfa4
Oyelowo <oyelowooyedayo@gmail.com>
2018-10-14 22:06:05 +0300

    Delete button Ioncion also styled and functional

--- src/components/PlaceDetail/PlaceDetail.js
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -9,7 +9,7 @@ import {
   TouchableOpacity
 } from "react-native";
 
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from "react-native-vector-icons/Ionicons";
 
 const placeDetail = props => {
   let modalContent = null;
@@ -35,10 +35,16 @@ const placeDetail = props => {
       <View style={styles.modalContainer}>
         {modalContent}
         <View>
-        <TouchableOpacity>
-            <Icon size={30} name="ios-trash" color="red" />
-          </ TouchableOpacity >
-          <Button title="Delete" color="red" onPress={props.onItemDeleted} />
+          <TouchableOpacity>
+            <View style={styles.deleteButton}>
+              <Icon
+                size={30}
+                name="ios-trash"
+                color="red"
+                onPress={props.onItemDeleted}
+              />
+            </View>
+          </TouchableOpacity>
           <Button title="Close" onPress={props.onModalClosed} />
         </View>
       </View>
@@ -58,6 +64,9 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     fontSize: 28
+  },
+  deleteButton: {
+    alignItems: "center"
   }
 });
 
