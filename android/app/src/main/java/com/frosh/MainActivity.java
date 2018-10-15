5d53475b71ff183e5c310ae22db13a219d370aac
Oyelowo <oyelowooyedayo@gmail.com>
2018-10-15 06:17:54 +0300

    react native navigator successfully installed and built

--- android/app/src/main/java/com/frosh/MainActivity.java
+++ android/app/src/main/java/com/frosh/MainActivity.java
@@ -1,15 +1,21 @@
 package com.frosh;
 
-import com.facebook.react.ReactActivity;
+// import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+ import com.reactnativenavigation.controllers.SplashActivity;
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "Frosh";
-    }
-}
+ public class MainActivity extends SplashActivity {
+
+ }
+ 
+// public class MainActivity extends ReactActivity {
+
+//     /**
+//      * Returns the name of the main component registered from JavaScript.
+//      * This is used to schedule rendering of the component.
+//      */
+//     @Override
+//     protected String getMainComponentName() {
+//         return "Frosh";
+//     }
+// }
