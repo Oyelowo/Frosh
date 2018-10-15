6c4ee9344902b1d9d217779cb05f4edd023af323
Oyelowo <oyelowooyedayo@gmail.com>
2018-10-15 08:46:21 +0300

    pushing  pages: navigating forward

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import { Navigation } from "react-native-navigation";
 import AuthScreen from "./src/screens/Auth/Auth";
-import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
-import FindPlaceScreen from './src/screens/FindPlace/FindPlace'
+import SharePlaceScreen from "./src/screens/SharePlace/SharePlace";
+import FindPlaceScreen from "./src/screens/FindPlace/FindPlace";
+import PlaceDetailScreen from "./src/screens/PlaceDetail/PlaceDetail";
+import { Provider } from "react-redux";
+import configureStore from "./src/store/configureStore";
 
+const store = configureStore();
 
 // Register Screens
-Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
-Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen);
-Navigation.registerComponent("awesome-places.FindPlaceScreen", () =>FindPlaceScreen);
+Navigation.registerComponent(
+  "awesome-places.AuthScreen",
+  () => AuthScreen,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "awesome-places.PlaceDetailScreen",
+  () => PlaceDetailScreen,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "awesome-places.SharePlaceScreen",
+  () => SharePlaceScreen,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "awesome-places.FindPlaceScreen",
+  () => FindPlaceScreen,
+  store,
+  Provider
+);
 
 // Start an App
 Navigation.startSingleScreenApp({
